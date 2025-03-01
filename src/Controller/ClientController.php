4d72d374603518f53cacd08bb55e76234efa6979
ghaith1a <ghaith.aissa@esprit.tn>
2025-03-01 12:05:21 -0100

    Ajout du projet Symfony sur la branche cours

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 use App\Entity\Cours;
 use App\Entity\Devoir;
+use App\Entity\Ratings;
 use App\Repository\CoursRepository;
 use App\Repository\DevoirRepository;
+use App\Repository\RatingsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/client')]
@@ -74,6 +78,47 @@ public function show1(Devoir $devoir): Response
         'devoir' => $devoir,
     ]);
 }
+#[Route('/course/{id}/rate', name: 'rate_course', methods: ['POST'])]
+    public function rateCourse(Request $request, EntityManagerInterface $em, int $id, RatingsRepository $ratingRepo): Response
+    {
+        $user = $this->getUser();
+        $course = $em->getRepository(Cours::class)->find($id);
+        
+        if (!$course) {
+            throw $this->createNotFoundException('Course not found');
+        }
+
+        $ratingValue = (int) $request->request->get('rating');
+        if ($ratingValue < 1 || $ratingValue > 5) {
+            return $this->json(['error' => 'Invalid rating value'], 400);
+        }
+
+        // Check if user already rated this course
+        $existingRating = $ratingRepo->findOneBy(['user' => $user, 'cours' => $course]);
+
+        if ($existingRating) {
+            $existingRating->setValue($ratingValue);
+        } else {
+            $rating = new Ratings();
+            $rating->setUser($user);
+            $rating->setCours($course);
+            $rating->setValue($ratingValue);
+            $em->persist($rating);
+        }
+
+        $em->flush();
+
+        // Calculate the average rating
+        $ratings = $ratingRepo->findBy(['cours' => $course]);
+        $averageRating = array_sum(array_map(function($rating) {
+            return $rating->getValue();
+        }, $ratings)) / count($ratings);
+
+        $course->setAverageRating($averageRating);
+        $em->flush();
+
+        return $this->json(['success' => 'Rating submitted']);
+    }
 
     
 }
---
+++ src/Entity/Ratings.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RatingsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RatingsRepository::class)]
+class Ratings
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $User = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Cours $Cours = null;
+
+    #[ORM\Column]
+    private ?int $value = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->User;
+    }
+
+    public function setUser(?User $User): static
+    {
+        $this->User = $User;
+
+        return $this;
+    }
+
+    public function getCours(): ?Cours
+    {
+        return $this->Cours;
+    }
+
+    public function setCours(?Cours $Cours): static
+    {
+        $this->Cours = $Cours;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
